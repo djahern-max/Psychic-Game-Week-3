a33674c9e7a0770124deec508263ca4ebd2c67aa
Dano <danielaherniv@gmail.com>
2019-07-14 23:30:50 -0400

    update

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -5,7 +5,7 @@ window.onload=function(){
 
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var guessesLeft = 9;
-var playersGuess = [2];
+var playersGuess = [];
 
 
 // The code below prompts the computer to gesss a letter within the "letters" array
@@ -24,18 +24,23 @@ document.getElementById("Computer's Guess").innerHTML = letterToGuess;
 // Create code that tracks the game player's guess
 
 
-document.onkeydown=function(){
+document.onkeydown=function(event) {
 
-  var e = event.key;
-  playersGuess.push(e);
+var key_press = String.fromCharCode(event.keyCode);
+var key_code = event.keyCode;
 
-  console.log(playersGuess);
+document.getElementById("kp").innerHTML = key_press; 
+document.getElementById("kc").innerHTML = key_code;
+var status = document.getElemmentByID("status");
+status.innerHTML = "DOWN Event Fired For : "+key_press;
 }
 
-document.getElementById("Players Guess").innerHTML = playersGuess;
 
 
+// var e = event.key;
+  // playersGuess.push(e);
 
+  // console.log(playersGuess);
 
 //Keep track of the computer' selection within the array
 
